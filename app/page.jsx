715445681b7784b8bd7a9a9b9d3225005173df1c
The Financier <itisenward@gmail.com>
2023-09-02 21:55:45 +0200

    Created simple home page section

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full flex-center flex-col'>
+        <h1 className='head_text text-center'>
+            Discover and Share
+            <br className='md:hidden' />
+            <span className='orange_gradient text-center'> AI-Powered Promts</span>
+        </h1>
+        <p className='desc text-center'>
+            Promptify is an open-source AI promting tool for modern world to discover, create and share creative promts
+        </p>
+    </section>
   )
 }
 
