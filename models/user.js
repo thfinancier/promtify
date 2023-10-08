9bbaebf5f35381597d4019d5ab6d6be5a48312f8
The Financier <itisenward@gmail.com>
2023-10-08 14:28:28 +0200

    Prompt model

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'User name is required'],
-        match: [/^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, 'Username invalid, it should contain 6-20 alphanumeric letters and be unique!']
+        match: [/^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 6-20 alphanumeric letters and be unique!"]
     },
     image: {
         type: String
